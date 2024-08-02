281dacdd134a72185c350a04ccad09b900fc5eea
YuriZoko <florian.girard07@orange.fr>
2024-08-02 17:11:51 +0200

    add username on header + sign out

--- src/components/header.js
+++ src/components/header.js
@@ -1,10 +1,18 @@
 import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faUserCircle } from '@fortawesome/free-solid-svg-icons';
+import { faUserCircle, faPerson } from '@fortawesome/free-solid-svg-icons';
 import argentBankLogo from '../img/argentBankLogo.png';
 import '../css/main.css';
-
+import { logout } from '../redux/authSlice';
 const Header = () => {
+  const dispatch = useDispatch();
+  const { user, token } = useSelector((state) => state.auth);
+
+  const handleLogout = () => {
+    dispatch(logout());
+  };
+
   return (
     <nav className="main-nav">
       <a className="main-nav-logo" href="/">
@@ -16,10 +24,23 @@ const Header = () => {
         <h1 className="sr-only">Argent Bank</h1>
       </a>
       <div>
-        <a className="main-nav-item" href="/sign-in">
-          <FontAwesomeIcon icon={faUserCircle} />
-          Sign In
-        </a>
+        {token && user ? (
+          <>
+            <span className="main-nav-item">
+              <FontAwesomeIcon icon={faPerson} />
+              {user.userName}
+            </span>
+            <a className="main-nav-item" href="/" onClick={handleLogout}>
+              <FontAwesomeIcon icon={faUserCircle} />
+              Sign Out
+            </a>
+          </>
+        ) : (
+          <a className="main-nav-item" href="/sign-in">
+            <FontAwesomeIcon icon={faUserCircle} />
+            Sign In
+          </a>
+        )}
       </div>
     </nav>
   );
